30c4aedc322053d9efafe922c8076be611480553
[200~JkjupiterKing <jupiterkingtechnologies@gmail.com>
2025-11-12 12:40:45 +0530

    Initial commit

---
+++ Smart_Travel_Connect_api/src/main/java/com/Smart_Travel_Connect_api/SmartTravelConnectApiApplication.java
@@ -0,0 +1,13 @@
+package com.Smart_Travel_Connect_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTravelConnectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTravelConnectApiApplication.class, args);
+	}
+
+}
---
+++ Smart_Travel_Connect_api/src/test/java/com/Smart_Travel_Connect_api/SmartTravelConnectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Smart_Travel_Connect_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTravelConnectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
